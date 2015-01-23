5411248a09981c9ed68f664e0cb7febb67b4cc11
Cosette Goldstein <cosette.goldstein@duke.edu>
2015-01-23 16:51:53 -0500

    new edits

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
 	public static void main(String args[]) {
 		
 		System.out.print("Helllo World HI IM GEORGIA");
-		System.out.print("bye world");
+		System.out.print("bye world DUH");
 		
 	}
 }
