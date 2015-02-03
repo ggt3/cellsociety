2fadf924aa235c5b840afeba25ec61adaa3f9be2
gtnet8@gmail.com <gtnet8@gmail.com>
2015-02-02 21:50:12 -0500

    added test simulation

---
+++ src/controller/TestSimulations.java
@@ -0,0 +1,62 @@
+package controller;
+import java.util.HashMap;
+
+import model.Cell;
+import model.CellState;
+import model.Grid;
+
+
+public class TestSimulations {
+	
+	public Grid stupidMakeLifeGrid() {
+		Grid newG = new Grid(4,4);
+		Cell s = new Cell(CellState.ALIVE);
+		Cell t=new Cell(CellState.EMPTY);
+		for (int i = 0; i < 4; i++) {
+			for (int k =0; k<4; k++) {
+				if (i==1)
+					newG.putCell(s, i, k);
+				else
+					newG.putCell(t, i, k);
+			}
+		}
+		return newG;
+	}
+	
+	public HashMap<String,String> stupidMap() {
+		HashMap<String,String> map = new HashMap<String, String>();
+		map.put("ALIVE", "RED");
+		map.put("EMPTY", "BLACK");
+		map.put("TREE", "AQUA");
+		map.put("BURNING", "RED");
+		return map;
+	}
+	
+	public Packager createMap(HashMap<String,String> d) {
+		Packager p = new Packager();
+		p.setColorMap(d);
+		return p;
+	}
+	//fire burning
+	public Packager createProperties() {
+		HashMap<String, Object> d = new HashMap<String, Object>();
+		d.put("THRESHOLD", 0.50);
+		Packager p = new Packager();
+		p.setPropertiesMap(d);
+		return p;
+	}
+	public Grid stupidMakeFireGrid() {
+		Grid newG = new Grid(4,4);
+		Cell s = new Cell(CellState.BURNING, createProperties());
+		Cell t=new Cell(CellState.TREE, createProperties());
+		for (int i = 0; i < 4; i++) {
+			for (int k =0; k<4; k++) {
+				if (i==1)
+					newG.putCell(s, i, k);
+				else
+					newG.putCell(t, i, k);
+			}
+		}
+		return newG;
+	}
+}
